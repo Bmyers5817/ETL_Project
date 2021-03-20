9d0bef4c33bf10168a717ae47f209bae1e240a28
Beth Myers <bmyers5817@gmail.com>
2021-03-20 11:56:07 -0400

    Push of successful jupyter notebooks and initial query

--- query.sql
+++ query.sql
@@ -1,15 +1,71 @@
 -- Create tables for raw data to be loaded into
-CREATE TABLE  (
-id INT PRIMARY KEY,
- TEXT,
- TEXT
+CREATE TABLE  regions(
+id SERIAL PRIMARY KEY,    
+noc TEXT,
+region TEXT,
+notes TEXT
 );
 
-CREATE TABLE  (
-id INT PRIMARY KEY,
- TEXT,
- TEXT
+CREATE TABLE  teams(
+id SERIAL PRIMARY KEY,
+athlete_id INT,
+team TEXT,
+noc TEXT
 );
 
+CREATE TABLE  athletes(
+id SERIAL PRIMARY KEY,
+athlete_id INT,
+name TEXT,
+sex TEXT,
+age float,
+height float,
+weight float
+);
 
+CREATE TABLE  sport(
+id SERIAL PRIMARY KEY,
+athlete_id INT,
+sport TEXT
+);
+
+CREATE TABLE  medals(
+id SERIAL PRIMARY KEY,    
+athlete_id INT,
+medal TEXT,
+event TEXT
+);
+
+CREATE TABLE  games(
+id SERIAL PRIMARY KEY, 
+noc TEXT,
+games TEXT,
+year TEXT,
+season TEXT,
+city TEXT
+);
+
+CREATE TABLE  venues(
+id SERIAL PRIMARY KEY, 
+city TEXT,
+country TEXT,
+year TEXT,
+continent TEXT,
+opening_ceremony TEXT,
+closing_ceremony TEXT
+);
+
+SELECT * FROM venues
+
+SELECT * FROM athletes
+
+SELECT * FROM games
+
+SELECT * FROM medals
+
+SELECT * FROM regions
+
+SELECT * FROM sport
+
+SELECT * FROM teams
 
